baaff4bb7603e0a97365ec617c113d277ec0182f
woynaw <51448060+woynaw@users.noreply.github.com>
2019-06-26 16:09:41 +0300

    Initial commit

---
+++ blog/articles/views.py
@@ -0,0 +1,7 @@
+#coding:utf-8
+from django.http import HttpResponse
+from .models import Article
+from django.shortcuts import render
+def archive(request):
+    return render(request, 'archive.html', {"posts":
+Article.objects.all()})
